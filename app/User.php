abc418aa1947a44e32c6d303131a586c73528212
kylerdanielster <kylerdanielster@gmail.com>
2017-09-11 12:02:35 -0500

    initialize cube and card model

--- app/User.php
+++ app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function cubes()
+    {
+        return $this->hasMany(Cube::class);
+    }
 }
