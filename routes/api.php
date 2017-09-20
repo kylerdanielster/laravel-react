e30415e4d6ad029073ecf2e081b44f305a40eb01
kylerdanielster <kylerdanielster@gmail.com>
2017-09-20 00:07:09 -0500

    Attaching an existing card works, but not a new one yet. Cleaned up the API routes file

--- routes/api.php
+++ routes/api.php
@@ -20,25 +20,23 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Could set up a custom controller CustomController@cubeCard
-//Route::get('/cube/{cube}/cards', 'CubeController@index');
+Route::resource('cube', 'CubeController');
+Route::resource('card', 'CardController');
+
 Route::get('/cube/{cube}/cards', function(Cube $cube) {
     return $cube->cards;    
 });
 
-//Route::get('/card/{card}', 'CardController@index');
 Route::get('/card/{card}', function (Card $card) {
     return $card;
 });
 
-// Leaving the request object for now, I may want this to be a post endpoint
-Route::get('/search/{name}', function (Request $request, $name) {
+Route::get('/search/{name}', function ($name) {
     $client = new GuzzleHttpClient();
 
     //$response = $client->request('GET',
     //    'https://api.scryfall.com/cards/named?fuzzy=' . $name);
 
-// Will want to use this to get all versions later
     $response = $client->request('GET',
         'https://api.scryfall.com/cards/search?q=%2B%2B%21' . " . $name . ");
 
@@ -46,4 +44,9 @@ Route::get('/search/{name}', function (Request $request, $name) {
 });
 
 // Endpoint for adding a card to a cube, using attach?
-//Route::post('/addCard'/
+Route::post('/addCard/{cube}/{card}', function(Cube $cube, $card) {
+    //return $card;
+    // need to get or create the card in the db then attach the card in the db
+    //$card = Card::where('id', $id)->firstOrCreate();  
+    return $cube->cards()->attach($card);
+});
